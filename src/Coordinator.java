c27f2c1858c0f8727565ea6a8e52f694607acd3b
sc4g18 <sc4g18@soton.ac.uk>
2020-05-21 21:09:58 +0100

    Added functionality to the Coordinator so that it accepts connection requests from participants.

--- src/Coordinator.java
+++ src/Coordinator.java
@@ -1,13 +1,20 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Coordinator extends Thread
 {
-	int port; // this coordinator is listening on
-	int lport; // logger server is listening on
-	int parts; // number of participants
-	int timeout; // timeout in milliseconds <- used when waiting for a message from a participant to decide whether that participant has failed.
-	String[] options; // set (no duplicates) of options
+	private final int coordinatorPort; // this coordinator is listening on
+	private final int loggerPort; // logger server is listening on
+	private final int parts; // number of participants
+	private final int timeout; // timeout in milliseconds <- used when waiting for a message from a participant to decide whether that participant has failed.
+	private final String[] options; // set (no duplicates) of options
 
+	private ServerSocket serverSocket;
+
+	private HashMap<Thread, Socket> participants = new HashMap<>();
 
 	public static void main(String[] args)
 	{
@@ -16,6 +23,7 @@ public class Coordinator extends Thread
 			Coordinator coordinator = new Coordinator(args);
 
 			// 1. WAIT FOR PARTICIPANTS to join <- message: "JOIN port"
+			coordinator.waitForParticipants();
 
 			// 2. SEND PARTICIPANT DETAILS to each participant <- message: "DETAILS [port]"
 
@@ -24,6 +32,10 @@ public class Coordinator extends Thread
 			// 4. RECEIVE VOTES from participants <- message: "OUTCOME outcome [port]"
 		}
 		catch(ArgumentQuantityException e)
+		{
+			e.printStackTrace();
+		}
+		catch(IOException e)
 		{
 			e.printStackTrace();
 		}
@@ -53,15 +65,38 @@ public class Coordinator extends Thread
 			throw new ArgumentQuantityException(args);
 		}
 
-		this.port = Integer.parseInt(args[0]);
-		this.lport = Integer.parseInt(args[1]);
+		this.coordinatorPort = Integer.parseInt(args[0]);
+		this.loggerPort = Integer.parseInt(args[1]);
 		this.parts = Integer.parseInt(args[2]);
 		this.timeout = Integer.parseInt(args[3]);
 		this.options = Arrays.copyOfRange(args, 4, args.length);
-
-		System.out.println("Running with C: " + this.port + ", L: " + this.lport + ", P: " + this.parts + ", T: "
+		System.out.println("Running with C: " + this.coordinatorPort + ", L: " + this.loggerPort + ", P: " + this.parts + ", T: "
 				           + this.timeout + ", O: " + Arrays.toString(this.options));
+
+		try
+		{
+			serverSocket = new ServerSocket(coordinatorPort);
+			System.out.println("Coordinator: Initialised listening on " + coordinatorPort + ", expecting " + parts + " participants, options: " + options.toString());
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		System.out.println("Server socket initialised");
 	}
 
+	/**
+	 * Wait for the required number of participants to join
+	 */
+	private void waitForParticipants() throws IOException
+	{
+		Socket socket;
+		while(participants.size() < parts)
+		{
+			socket = serverSocket.accept();
+			socket.setSoLinger(true, timeout); // <-------------------------------------------------------------------- not too sure about this
 
+			// CREATE THE THREAD TO HANDLE THE PARTICIPANT + ADD IT TO THE MAP
+		}
+	}
 }
